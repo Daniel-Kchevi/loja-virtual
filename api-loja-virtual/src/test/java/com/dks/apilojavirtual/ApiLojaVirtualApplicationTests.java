368425e4be909037ca5fc6315f4e8043f8ab88ad
Daniel Kchevi <dkchevi@hotmail.com>
2020-06-27 13:36:55 -0300

    estrutura api

---
+++ api-loja-virtual/src/test/java/com/dks/apilojavirtual/ApiLojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dks.apilojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
